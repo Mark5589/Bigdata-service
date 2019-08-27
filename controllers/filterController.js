07d986289c22c6eb3e9ad2f9a0c5c0f203d89128
Mark Gurin <kaseopia.way@gmail.com>
2019-08-27 12:13:02 +0300

    fixed filterData page, graph render

--- controllers/filterController.js
+++ controllers/filterController.js
@@ -12,13 +12,13 @@ const { Parser } = require('json2csv');
 const bigml = require('bigml');
 const async = require('async');
 let URLDownload = [];
-
 let resBatchFile = {
   cluster0:[],
   cluster1:[],
   cluster2:[],
   cluster3:[]
 };
+
 
 const util = require('util')
 
@@ -32,7 +32,7 @@ const fetch = require('node-fetch')
 
 exports.filter_main = (req, res) => {
     // render the files that uploaded to mongo
-    res.render('filter');
+    res.render('filter', {a_style: 'display: none;'});
 };
 
 exports.search_result = (req, res, next) => {
@@ -43,44 +43,36 @@ exports.search_result = (req, res, next) => {
     debug(fromQuery + ' --__--> ' + toQuery);
     Retail.find({date : {$gte: new Date(fromQuery), $lte: new Date(toQuery) }}, {retail_id: 1, customer_id: 1, items: 1} , (err, docs)=>{
         if (err) {return next(err); }
-
-        // Save the mongo Query to csv file
-        // mongoDBResponse2csv(docs)
-
-        // run ML on the csv file
-        // bigMLController();
-        
-        // api_Controllers(docs)
+        docs.forEach(e => debug(e['retail_id']));
         async.series([
+            // async.apply(sleep, 3000)
             async.apply(mongoDBResponse2csv, docs),
-            async.apply(bigMLController,  './public/outCSVs/mongoDBQuery.csv'),
-            async.apply(readFileCSV)
+            async.apply(bigMLController,  './public/outCSVs/mongoDBQuery.csv')  
         ], 
         function(err, result){
             if(err) {return next(err);}
-          //   res.render('resGraph', 
-          //   {
-          //     cluster0_size: resBatchFile['cluster0'].length,
-          //     cluster0_text:  resBatchFile['cluster0'].join(),
-          //     cluster1_size: resBatchFile['cluster1'].length,
-          //     cluster1_text:  resBatchFile['cluster1'].join(),
-          //     cluster2_size: resBatchFile['cluster2'].length,
-          //     cluster2_text:  resBatchFile['cluster2'].join(),
-          //     cluster3_size: resBatchFile['cluster3'].length,
-          //     cluster3_text:  resBatchFile['cluster3'].join()
-          // });
-          res.render('filter', {resLink: true})
+          res.render('filter', {a_style: 'display: inline;'})
         });
-   
     });
 }
 
-// cluster1_text: 'hello',
-// cluster2_text: 'hello',
-// // cluster3_text: 'hello'
-// cluster1_size: resBatchFile['cluster1'].length,
-// cluster2_size: resBatchFile['cluster2'].length,
-// cluster3_size: resBatchFile['cluster3'].length,
+exports.show_graph_results = (req, res, next) => {
+
+    async.series([readFileCSV], function(err, result){
+      if(err) {return next(err);}
+      res.render('resGraph', 
+      {
+        cluster0_size: resBatchFile['cluster0'].length,
+        cluster0_text:  resBatchFile['cluster0'].join(),
+        cluster1_size: resBatchFile['cluster1'].length,
+        cluster1_text:  resBatchFile['cluster1'].join(),
+        cluster2_size: resBatchFile['cluster2'].length,
+        cluster2_text:  resBatchFile['cluster2'].join(),
+        cluster3_size: resBatchFile['cluster3'].length,
+        cluster3_text:  resBatchFile['cluster3'].join()
+    });      
+    });
+};
 const downloadURL  = async  (url, pathto) => {
     debug('Downloading From: ' + url);
     // url ='https://bigml.io/andromeda/batchcentroid/5d615668529963040900fd33/download?username=mark5589;api_key=8e12ffdb67a89003140d736bf63a055bbde9f2a1'
@@ -107,9 +99,6 @@ const download_file_wget = function(file_url, cb) {
 
 const bigMLController = (pathtoFile, cb) => {
     debug('request to bigML');
-    //source.create('./public/ml/test.csv', function(error, sourceInfo) 
-    const filepath = '/Users/recon/Documents/studies/year3/Semester-B/4-DATA-Big data/1.1-project/bigData-proj/public/outCSVs/mongoDBQuery.csv';
-    const path2 = '/Users/recon/Documents/studies/year3/Semester-B/4-DATA-Big data/1.1-project/bigData-proj/public/ml/test.csv';
     connection = new bigml.BigML('cyberpunks5512', 'b3d538219d7b18fd87eb04db7d46386aeb344e28');
     var source = new bigml.Source();
     source.create(pathtoFile, function(error, sourceInfo) {
@@ -128,12 +117,9 @@ const bigMLController = (pathtoFile, cb) => {
                     const httpsPrefix = 'https://bigml.io/andromeda/';
                     const httpRequest = httpsPrefix + batchCentroidResourse + download_api_string;
                     debug("URL to BATCH:" + httpRequest + '<<<');
-                    URLDownload.push(httpRequest);
-                    // downloadURL (httpRequest, './public/outCSVs/downloadedBatch');
-                    // download_file_wget(httpRequest)
+                    URLDownload.push(httpRequest);                  
                     sleep(10000, () => {
                         request(httpRequest).pipe(fs.createWriteStream('./public/outCSVs/batchCentroidRes.csv'));
-
                     })
                     
                     // https://bigml.io/andromeda/batchcentroid/5d5d072de47684734100d51a/download?username=mark5589;api_key=8e12ffdb67a89003140d736bf63a055bbde9f2a1
@@ -158,11 +144,9 @@ const mongoDBResponse2csv = (res, cb) => {
     fs.writeFile('./public/outCSVs/mongoDBQuery.csv', csv, (err)=>{
         if (err) throw err;
         
-       // return './public/outCSVs/mongoDBQuery.csv'
+       
        cb(null);
-    })
-    
-    // debug(csv);
+    });
 }
 
 const readFileCSV = (cb) => {
@@ -170,6 +154,11 @@ const readFileCSV = (cb) => {
   resBatchFile['cluster1'] = [];
   resBatchFile['cluster2'] = [];
   resBatchFile['cluster3'] = [];
+  // var stop = new Date().getTime();
+    // while(new Date().getTime() < stop + 10000) {
+    //     ;
+    // }
+ 
 
   // let tmp = 'cluster0';
   // resBatchFile[tmp].push('hello');
--- routes/filter.js
+++ routes/filter.js
@@ -6,7 +6,9 @@ const filter_controller = require('../controllers/filterController');
 
 router.get('/', filter_controller.filter_main);
 
-router.post('/filteredData', filter_controller.search_result);
+router.post('/', filter_controller.search_result);
+
+router.get('/filteredData', filter_controller.show_graph_results);
 
 // router.get('/tst/:id', (req, res) => {
 //     res.send(req.params.id)
